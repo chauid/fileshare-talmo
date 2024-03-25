3e4b01c0e67a34e16c53f073c4c1b429cf1d739a
chauid <dlfdl28@naver.com>
2024-03-25 10:17:07 +0900

    prettier, eslint 및 프로젝트 전역 설정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,25 @@
-import type { Config } from "tailwindcss";
+/* eslint-disable global-require */
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ['node_modules/flowbite-react/lib/esm/**/*.js', './components/**/*.{ts,tsx}', './app/**/*.{ts,tsx}'],
   theme: {
+    screens: {
+      xxs: '384px',
+      xs: '480px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      xxl: '1440px',
+    },
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
-  plugins: [],
+  plugins: [require('flowbite/plugin')],
 };
 export default config;
